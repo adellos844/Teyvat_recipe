a548a0d40bc6e446f29cc1f41e290edb79826eec
adellos844 <adellos844@g.educaand.es>
2026-05-27 11:56:49 +0200

    Creación del admin.py (y superuser), views.py y ediciones en el admin para la adición de imágenes

--- wiki_recetas/recetario_de_teyvat/admin.py
+++ wiki_recetas/recetario_de_teyvat/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Receta, Categoria, Comentario
 
-# Register your models here.
+@admin.register(Receta)
+class RecetaAdmin(admin.ModelAdmin):
+    list_display = ('Título', 'Región', 'Rareza', 'Autor', 'Fecha_de_creación')
+    list_filter = ('Región', 'Rareza', 'Fecha_de_creación')
+    search_fields = ('Título', 'Ingredientes', 'Autor__username')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('Nombre', 'Descripción')
+    search_fields = ('Nombre',)
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('Autor', 'Receta_asociada', 'Fecha_de_publicación')
+    list_filter = ('Fecha_de_publicación',)
+    search_fields = ('Contenido', 'Autor__username', 'Receta_asociada__Título')
--- wiki_recetas/recetario_de_teyvat/views.py
+++ wiki_recetas/recetario_de_teyvat/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Receta
 
-# Create your views here.
+def lista_recetas(request):
+    recetas = Receta.objects.all().order_by('-Fecha_de_creación')
+    return render(request, 'recetario/lista_recetas.html', {'recetas': recetas})
+
+def detalle_receta(request, pk):
+    receta = get_object_or_404(Receta, pk=pk)
+    comentarios = receta.comentario_set.all().order_by('-Fecha_de_publicación')
+    
+    contexto = {
+        'receta': receta,
+        'comentarios': comentarios
+    }
+    return render(request, 'recetario/detalle_receta.html', contexto)
